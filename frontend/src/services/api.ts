ee4d11240a403f4b051619e1a11f9e61bfce8eba
iaman8055 <iamanchauhan93@gmail.com>
2026-06-06 01:07:02 +0530

    some minor change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { mockDb } from "./mockDb";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 // Utility to helper direct call or wrap mockDb
